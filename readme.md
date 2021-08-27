10541e44bea1ce98220d13a60665c8552e99c0b6
Lais <lais@pop-os.localdomain>
2021-08-26 22:23:14 -0300

    all tests ok

--- readme.md
+++ readme.md
@@ -5,10 +5,10 @@
 ### Requisitos
 
 - [x] Criar usuário com <span style="color:tomato"> name </span>e o <span style="color:tomato">username</span>
-- [ ] Criar um novo todo
-- [ ] Listar todos os todos
-- [ ] Alterar o <span style="color:tomato"> title </span>e o <span style="color:tomato">deadline</span>
-- [ ] Marcar um todo como feito
-- [ ] excluir um todo
+- [x] Criar um novo todo
+- [x] Listar todos os todos
+- [x] Alterar o <span style="color:tomato"> title </span>e o <span style="color:tomato">deadline</span>
+- [x] Marcar um todo como feito
+- [x] excluir um todo
 
 > Observações: o username deve ser passado pelo header
--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const { username } = request.header;
+  const { username } = request.headers;
 
   const user = users.find(user => user.username === username);
 
   if(!user) {
-    return response.status(400).json({
+    return response.status(404).json({
       error: 'Esse usuário não existe!'
     })
   }
@@ -48,23 +48,79 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  user.todos.push({
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  })
+
+  return response.status(201).json(user.todos[user.todos.length-1]);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+  const { id } = request.params;
+
+  var todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) return response.status(404).json({ error: "Tarefa não encontrada" })
+
+  const idx = user.todos.indexOf(todo);
+
+  user.todos.splice(idx, 1);
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  user.todos.push(todo);
+
+  return response.json(user.todos[user.todos.length-1]);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  var todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) return response.status(404).json({ error: "Tarefa não encontrada" })
+
+  const idx = user.todos.indexOf(todo);
+
+  user.todos.splice(idx, 1);
+
+  todo.done = true;
+
+  user.todos.push(todo);
+
+  return response.json(user.todos[user.todos.length-1]);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  var todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) return response.status(404).json({ error: "Tarefa não encontrada" })
+
+  const idx = user.todos.indexOf(todo);
+
+  user.todos.splice(idx, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
